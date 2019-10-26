304991aa4eac5cd4ba0f5ffa5ed88c3e1c98456a
Neha695 <nehaanil695@gmail.com>
2019-10-26 01:40:03 -0700

    msg

---
+++ session1/js1.js
@@ -0,0 +1,10 @@
+function myFunction() {
+   //console.log(elem)
+   // elem.style.color="red"
+//function sayHi(element,eve){
+    //alert("HI")
+//debugger
+document.getElementById("changed").innerHTML=event.target.value
+console.log(event.target.value)
+//console.log(eve.clientX+ ':'+eve.clientY)
+}
